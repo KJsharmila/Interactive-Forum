ac7a2fa1d3d7c0a9950837b909ab797069389e84
KJsharmila <sharmila437@gmail.com>
2015-02-18 12:47:09 +0530

    user clicks on announcement it will redirect to announcement forum page

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150217044222) do
+ActiveRecord::Schema.define(version: 20150218064548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "announcements", force: true do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"

30f07dc26971ac824c87d252803a60dc2b036c56
Kedarnag Mukanahallipatna <kedarnag@Kedarnags-MacBook-Pro.local>
2015-03-03 09:22:48 -0800

    [master] Made changes to the application.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,10 +10,10 @@ set :log_level, :debug
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-role :app, %w{deploy@interactiveforum.qwinixtech.com}
-role :web, %w{deploy@interactiveforum.qwinixtech.com}
-role :db, %w{deploy@interactiveforum.qwinixtech.com}
-server 'interactiveforum.qwinixtech.com', roles: %w{:web, :app, :db}, user: 'deploy'
+role :app, %w{deploy@52.11.59.39}
+role :web, %w{deploy@52.11.59.39}
+role :db, %w{deploy@52.11.59.39}
+server '52.11.59.39', roles: %w{:web, :app, :db}, user: 'deploy'
 
 set :ssh_options, {
    #verbose: :debug,
